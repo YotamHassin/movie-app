2fb6b1fc07570c09f09561aef822e1b8f6f76d0d
yotamhassin <yotamhassin@gmail.com>
2022-03-28 20:12:11 +0300

    D:\mongodb-express-node\index.js Edit userModule. register autheticate

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ async function run() {
 	const { run, ObjectId } = require('./lib/mongoose');
 	const mongoose = await run(myConfig.mongodbRoute);
 
+
 	//.catch(reason => console.log('catch error in mongoose', reason))
 	//.then(() => console.log('then in mongoose'));
 	//.finally(() => console.log('finally in run function'));
@@ -30,7 +31,9 @@ async function run() {
 
 	//const moviesApi = require('./lib/movies');
 
-	const socketIO = require('socket.io')
+	const socketIO = require('socket.io');
+
+	//const path = require('path');
 
 	const http = require('http');
 
@@ -42,20 +45,20 @@ async function run() {
 
 	const axios = require('axios').default;
 
-	const path = require('path');
 
 	// 1)register 2)Autheticate 3)resetPassword
 	const username = {
-		_id: ObjectId("620709fc2006c10c1cab3d20"),
+		_id: ObjectId("6241e7c96f171313488f0e97"),
 		username: "test",
 		password: "12354"
 	}
+
 	// todo: 'invalid'
-	const fake = {
+	const full = {
 		...username,
 		email: "test@test.com",
 		firstName: "Manish",
-		lastName: "Ranjan",
+		/* lastName: "Ranjan",
 		middleName: "mi",
 		identificationNumber: "55555",
 		phoneNumber1: "5555555",
@@ -67,10 +70,10 @@ async function run() {
 			state: "NC",
 			city: "Cary",
 			country: "USA"
-		}
+		} */
 	};
 
-	//await userModule.register(fake);
+	//await userModule.register(full);
 	console.log('user Autheticate', await userModule.autheticate(username));
 
 	// App
@@ -126,6 +129,7 @@ async function run() {
 		methods: { get: true }
 	};
 
+	// query
 	//app.use('/interpolation/*', express.static(path.join(__dirname, 'build'), { index: 'index.html' }));
 	//app.use(express.static("public/ftp", {"index": ["default.html", "default.htm"]}))
 	// http://127.0.0.1:3000/getSingle?id=5
@@ -146,6 +150,7 @@ async function run() {
 	});
 
 	console.log('app.use static');
+
 	var options = {
 		dotfiles: 'ignore',
 		etag: false,
@@ -160,34 +165,14 @@ async function run() {
 
 	//app.use(express.static('public', options))
 
-	// app.use(express.static("public/ftp", {"index": ["default.html", "default.htm"]}))
-	var build = express.static(__dirname + "/public/build",
-		{
-			maxAge: '1h', index: 'index.html', fallthrough: true, redirect: false
-		});
-	app.use('/build', (req, res, arg1, arg2, arg3) => {
-		console.log('req, res, arg1, arg2, arg3');
-		return build(req, res, arg1, arg2, arg3);
-	});
-	app.use('/build', build);
-
-	var IOTest = express.static(__dirname + "/public/io-test",
-		{
-			maxAge: '1h', index: 'index.html', fallthrough: true
-		});
-	app.use('/io-test', IOTest);
-
-	app.use('/tmp', express.static(__dirname + "/public/dist"));
-
-	app.use(express.static("public/dist"));
-
 	// Home url
 	// /user/:name => req.params.name
 	app.get("/", (req, res) => {
 		// api result
 		//res.json({message: "Notetaking API v1"});
-		res.sendFile(path.join(__dirname, 'ClientApp', 'dist', 'index.html'));
-		//res.redirect('/index.html');
+		//res.sendFile(path.join(__dirname, 'ClientApp', 'dist', 'index.html'));
+
+		res.redirect('/index.html');
 
 	});
 
@@ -209,7 +194,7 @@ async function run() {
 	}
 
 	// http://127.0.0.1:3000/getSingle?id=5
-	app.get('/interpolation4/*', async function (req, res) {
+	app.get('/interpolation', async function (req, res) {
 		try {
 			//modify the url in any way you want
 			var locationName = 'interpolation';
@@ -217,15 +202,15 @@ async function run() {
 			const newurl = 'http://127.0.0.1:3001/' + locationName;
 			//const newurl = '127.0.0.1:3001/'+locationName;
 
-			console.log('app.get interpolation', { locationName, newurl });
+			console.log('app.get interpolation', {locationName, newurl});
 
 			const toPipe = request.get(newurl);
 			if (toPipe) {
 				console.log('if toPipe');
 				//return toPipe.pipe(res);
 				const body = toPipe.body;
-
-
+				
+				
 				res.pipe(toPipe);
 			}
 
@@ -358,6 +343,24 @@ async function run() {
 	});
 
 
+	console.log('app.use static');
+	var options = {
+		dotfiles: 'ignore',
+		etag: false,
+		extensions: ['htm', 'html'],
+		index: false,
+		maxAge: '1d',
+		redirect: false,
+		setHeaders: function (res, path, stat) {
+			res.set('x-timestamp', Date.now())
+		}
+	}
+
+	//app.use(express.static('public', options))
+
+	// app.use(express.static("public/ftp", {"index": ["default.html", "default.htm"]}))
+	app.use(express.static("ClientApp/dist"));
+
 	// public/images/kitten.jpg
 	// http://localhost:3000/static/images/kitten.jpg
 	//app.use('/static', express.static('public'));
@@ -411,106 +414,11 @@ in a browser to see the output`);
 	//chat(io);
 
 	console.log('Init Socket IO');
-
-	const connectedUserMap = new Map([[Number.MAX_VALUE, { status: 'online', name: 'admin' }]]);
-
 	io.on('connection', (socket) => {
-		const connectedUserId = socket.id;
-		const connectedUser = () => {
-			let user = connectedUserMap.get(connectedUserId);
-			return user ? (user.name ? user.name : connectedUserId) : connectedUserId;
-		};
+		/* … */
+		console.log(`on connection, socket`);
 
-		console.log(`user ${connectedUser()} connected, connection socket.`, socket.rooms);
-
-		//add property value when assigning user to map
-		connectedUserMap.set(connectedUserId, { status: 'online', name: '' });
-
-		const leave = (room = "room 237", emitText = `user ${connectedUser()} has left the room`) => {
-			socket.leave(room);
-			io.to(room).emit(emitText);
-			console.log(emitText + `, room: ${room}`);
-		}
-
-		const join = (room = "room 237", emitText = `user ${connectedUser()} has joined the room`) => {
-			socket.join(room);
-			io.to(room).emit(emitText);
-			console.log(emitText + `, room: ${room}`);
-		}
-
-		/* Rooms */
-		//console.log('rooms', socket.rooms); // Set { <socket.id> }
-		//join("room1"); //leave
-		//console.log('join room1 rooms', socket.rooms); // Set { <socket.id>, "room1" }
-
-		const chatMessageType = {
-			ChatMessage: 'chat message',
-			welcome: 'welcome',
-			join: 'join',
-			disconnect: 'disconnect',
-			message: 'message',
-			recieveUserName: 'recieveUserName',
-			callTest: 'callTest',
-
-		}
-
-		/* recieve User Name */
-		socket.on(chatMessageType.recieveUserName, function (name = '') {
-			socket.name = name
-			//find user by there socket in the map the update name property of value
-			let user = connectedUserMap.get(connectedUserId);
-			if (user) {
-				user.name = name;
-			}
-			io.emit('chat message', { type: chatMessageType.recieveUserName, id: connectedUserId, name }); // This will emit the event to all connected sockets
-		});
-
-		/* 
-typeof(() => {});
-'function'
-typeof({});
-'object'
-typeof(function (params) { });
-'function'
-typeof(typeof);
-VM165:1 Uncaught SyntaxError: Unexpected token ')'
-typeof(typeof);
-VM171:1 Uncaught SyntaxError: Unexpected token ')'
-typeof(this);
-'object'
-typeof([]);
-'object'
- */
-		function mapToStr(obj = {}) {
-			console.log(`mapToStr`);
-			if (obj !== undefined) {
-				console.log(`mapToStr obj`, obj);
-				if (typeof (obj) === 'object' && obj.length && obj.length > 0) {
-					console.log(`typeof (obj) === 'arr'`);
-					for (let index = 0; index < obj.length; index++) {
-						obj[index] = mapToStr(obj[index]);
-					}
-				}
-				else if (typeof (obj) === 'object') {
-					console.log(`typeof (obj) === 'object'`);
-					for (const key in obj) {
-						const element = obj[key];
-						if (element) {
-							if (typeof (element) === 'function') {
-								var toString = element.toString();
-								obj[key] = { type: 'func', toString };
-							}
-						}
-					}
-				}
-
-				else {
-
-				}
-			}
-
-			return obj;
-		}
+		io.sockets.emit('message', { type: 'new user' });
 
 		/* call Test */
 		socket.on(chatMessageType.callTest, function (name = '') {
@@ -527,7 +435,7 @@ typeof([]);
 		/* welcome connectedUserId */
 		//io.to(connectedUserId).emit('chat message', `you welcome: ${connectedUserId} to chat app`);
 		io.to(connectedUserId).emit('chat message', { type: chatMessageType.welcome, id: connectedUserId });
-		
+
 		/* broadcast connectedUserId joined */
 		//socket.broadcast.emit('chat message', `say welcome to: ${connectedUserId}`);
 		socket.broadcast.emit('chat message', { type: chatMessageType.join, id: connectedUserId });
@@ -551,6 +459,7 @@ typeof([]);
 		//socket.broadcast.emit('chat message', 'hi from user');
 
 		//io.sockets.emit('message', { type: 'new user' });
+
 
 
 		//socket.on('getMessages', () => this.getMessages());
